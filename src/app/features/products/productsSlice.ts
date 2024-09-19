7d5398da38d646843d6b62fe228dbab9a3591ec1
SolomDev00 <ew11solom@gmail.com>
2024-09-19 15:49:31 +0300

    ProductsSlice is done.

--- src/app/features/products/productsSlice.ts
+++ src/app/features/products/productsSlice.ts
@@ -1,20 +1,57 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { IProduct } from "../../../interfaces";
-import { RootState } from "../../store";
+import axiosInstance from "../../../config/axios.config";
 
 interface ProductsState {
+  loading: boolean;
   productsList: IProduct[];
+  error: null;
 }
 
 const initialState: ProductsState = {
+  loading: true,
   productsList: [],
+  error: null,
 };
+
+const getProductsList = createAsyncThunk(
+  "products/getProductsList",
+  async (_, thunkAPI) => {
+    const { rejectWithValue } = thunkAPI;
+
+    // *** Get Request
+    try {
+      const { data } = await axiosInstance.get(
+        "/products?limit=12&select=title,price,thumbnail"
+      );
+      return data;
+    } catch (error) {
+      return rejectWithValue(error);
+    }
+  }
+);
 
 const productsSlice = createSlice({
   name: "products",
   initialState,
   reducers: {},
+  extraReducers: {
+    // *** Pending
+    [`${getProductsList.pending}`]: (state) => {
+      state.loading = true;
+    },
+    // *** Fulfilled
+    [`${getProductsList.fulfilled}`]: (state, action) => {
+      state.loading = false;
+      state.productsList = action.payload;
+    },
+    // *** Rejected
+    [`${getProductsList.rejected}`]: (state, action) => {
+      state.loading = false;
+      state.productsList = [];
+      state.error = action.payload;
+    },
+  },
 });
 
-export const cartSelector = (state: RootState) => state.cart;
 export default productsSlice.reducer;
